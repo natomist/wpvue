f0db84d89debae096dc2383bac3ef25bb681f15b
Yevgen Zhuchij <natomist@gmail.com>
2018-01-29 10:57:29 +0200

    iview is removed

--- index.php
+++ index.php
@@ -4,15 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<?php
-	if( isset($_REQUEST['set']) and $_REQUEST['set'] == 'uikit' ) {
-		do_action('vue-output-script', 'uk-demo');
-	} elseif( isset($_REQUEST['set']) and $_REQUEST['set'] == 'iview' ) {
-		do_action('vue-output-script', 'demo-iview');
-	} else {
-		do_action('vue-output-script', 'index');
-	}
-?>
+	<?php do_action('vue-output-script', 'index'); ?>
 </head>
 <body>
 	<div id="app"></div>
