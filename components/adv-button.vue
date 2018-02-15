bc33665286a4ba0e036ae3733243fb1dac455f53
Yevgen Zhuchij <natomist@gmail.com>
2018-02-15 10:33:01 +0200

    1. new component adv-button 2. Draft of new component cookies-notify

---
+++ components/adv-button.vue
@@ -0,0 +1,11 @@
+<template>
+	<router-link :to="{name: name}" class="uk-button" v-text="name" />
+</template>
+
+<script>
+module.exports = {
+	props: [
+		'name',
+	],
+}
+</script>
---
+++ components/cookies-notify.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="cookies-notify uk-overlay-primary uk-text-center uk-padding-small 
+		uk-position-fixed uk-position-z-index uk-position-bottom uk-overlay ">
+		By using the {{name}} website you agree to our use of cookies as described in our cookie policy.
+		<a href="#">More</a> &nbsp;
+		<button class="uk-button uk-button-primary uk-button-small uk-border-rounded">Понял</button>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	props: ['name'],
+}
+</script>
